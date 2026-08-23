e1d9890ea1d95b79a1b86288982a64a01522e868
Abbas Baniyan <abbasbaniyan307@gmail.com>
2026-08-24 00:09:58 +0530

    ip

--- main.py
+++ main.py
@@ -407,9 +407,7 @@ from botocore.exceptions import BotoCoreError, ClientError
 # Initialize AWS Session
 def get_aws_session():
     return boto3.Session(
-        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-        region_name=os.getenv("AWS_DEFAULT_REGION", "us-east-1")
+        region_name=os.getenv("AWS_DEFAULT_REGION", "eu-north-1")
     )
 
 # -----------------------------------------------------------------------------
